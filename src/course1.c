7258cdda4349b2d1d6479ef15b2f65199bf84112
Danierays <65251073+Danierays@users.noreply.github.com>
2021-12-15 18:57:10 +0100

    Update course1.c

--- src/course1.c
+++ src/course1.c
@@ -1,13 +1,3 @@
-/******************************************************************************
- * Copyright (C) 2017 by Alex Fosdick - University of Colorado
- *
- * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
- * permitted to modify this and use it to learn about the field of embedded
- * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
- *
- *****************************************************************************/
 /**
  * @file course1.c 
  * @brief This file is to be used to course 1 final assessment.
